75c2afcf918a71cdecf4f38dfb1db18378cec7d0
erik_dussel <erikdussel@gmail.com>
2018-06-03 20:58:52 +0200

    made more responsive, changed landingpage, improved navigation, introduced some bugs

--- js/navigation.js
+++ js/navigation.js
@@ -3,54 +3,84 @@ $.get('components/lander.html', (data) => {
 });
 $.get('components/navigation.html', (data) => {
 	$('#navigation-container').replaceWith(data);
+	$('#navigation-container').hide();
 });
 
 $(document).ready( () => {
-	$('#buyer-segment').on('click', () => {
-		$.get('components/account.html', (data) => {
+	$('#lander-search-button').on('click', () => {
+		$.get('components/search.html', (data) => {
 			$('#content-container').replaceWith(data);
-		})	
+			$('#content-container').hide();
+			$('#search-page').addClass('active');
+		})
+		$('#navigation-container').slideDown(1500, () => {
+			$('#content-container').show();
+			scrollToContent();
+		})
 	})
-	$('#seller-segment').on('click', () => {
-		$('#navigation-container').show();
+	$('#lander-login-button').on('click', () => {
 		$.get('components/account.html', (data) => {
 			$('#content-container').replaceWith(data);
-		})	
+			$('#content-container').hide();
+		})
+		$('#navigation-container').slideDown(1500, () => {
+			$('#content-container').show();
+			scrollToContent();
+		});
 	})
 
 	$('#search-page').on('click', () => {
 		$.get('components/search.html', (data) => {
 			$('#content-container').replaceWith(data);
-		})	
+		})
+		scrollToContent();
+		$('#search-page').addClass('active');
+		$('#search-page').siblings().removeClass('active');	
 	})
 	$('#info-page').on('click', () => {
 		$.get('components/info.html', (data) => {
 			$('#content-container').replaceWith(data);
 		})	
+		scrollToContent();
+		$('#info-page').addClass('active');
+		$('#info-page').siblings().removeClass('active');
 	})
 
 	$('#contact-page').on('click', () => {
 		$.get('components/contact.html', (data) => {
 			$('#content-container').replaceWith(data);
 		})	
+		scrollToContent();
+		$('#contact-page').addClass('active');
+		$('#contact-page').siblings().removeClass('active');
 	})
 
 	$('#about-page').on('click', () => {
 		$.get('components/about.html', (data) => {
 			$('#content-container').replaceWith(data);
-		})	
+		})
+		scrollToContent();
+		$('#about-page').addClass('active');
+		$('#about-page').siblings().removeClass('active');	
 	})
 
 	$('#account-page').on('click', () => {
 		$.get('components/account.html', (data) => {
 			$('#content-container').replaceWith(data);
 		})
+		scrollToContent();
 	})
 
 	$('#milestone-page').on('click', () => {
 		$.get('components/milestone.html', (data) => {
 			$('#content-container').replaceWith(data);
 		})
+		scrollToContent();
 	})
 });
+
+function scrollToContent()
+{
+	$('html, body').animate({scrollTop: $('#content-container').offset().top -50}, 1000);
+}
 
