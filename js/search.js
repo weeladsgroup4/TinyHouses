b2d9c449a5846ed64c41260a4ae204fb6e7608d6
erik_dussel <erikdussel@gmail.com>
2018-06-05 13:14:02 +0200

    tiny changes to search

--- js/search.js
+++ js/search.js
@@ -3,13 +3,26 @@ var priceSlider = document.getElementById("priceSlider");
 var priceOutput = document.getElementById("priceText");
 var sizeSlider = document.getElementById("distanceSlider");
 var sizeOutput = document.getElementById("distanceText");
+var cityOnly = false;
 
-window.load = $.getJSON('../wee_mock_data.json', (data) => {
-							houseData = data;
-							for (var i = 0; i < houseData.length; i++) {
-								populateResults(houseData[i], i);
-							}
-						});
+$(document).ready(()=> {
+	$.getJSON('../wee_mock_data.json', (data) => {
+		houseData = data;
+		for (var i = 0; i < houseData.length; i++) {
+			populateResults(houseData[i], i);
+		}
+	});
+	$('input[type=checkbox]').on("click", ()=>{
+		if (cityOnly) {
+			cityOnly = false;
+		} 
+		else {
+			cityOnly = true;
+		}
+		$('#price-container').children().toggle();
+		$('#size-container').children().toggle();
+	});
+}) 
 
 priceOutput.innerHTML = priceSlider.value;
 sizeOutput.innerHTML = sizeSlider.value;
@@ -27,7 +40,9 @@ function getHouses(size, price, location) {
 	for (var i = 0, j = 0; i < houseData.length; i++) {
 		house = houseData[i];
 		city = house.city.toUpperCase();
-		if (house.size <= size && house.price <= price && (location == '' || location == city)) {
+		checkCity = (location == '' || location == city);
+		checkSizePrice = (house.size <= size && house.price <= price);
+		if ((checkSizePrice && checkCity) || (cityOnly && checkCity)) {
 			populateResults(house, j);
 			j++;
 		}
@@ -47,7 +62,7 @@ function search() {
 function populateResults(house, i) {
 	$('.search-content-container').append(
 		// ignore this line
-		'<div class="col col-sm-4"><img src="https://www.1limburg.nl/sites/default/files/public/styles/media-paragraph/public/macy-miller-tiny-house-2.jpg?itok=ySmiqYLu" class="img-fluid img-thumbnail"/><div class="row row-padding"><div class="col-sm-6"><p><span class="fa fa-map-pin"></span>'+house.city+'</p></div><div class="col-sm-6"><p>'+house.longitude + ', ' + house.latitude + '</p></div></div><div class="row row-padding"><div class="col-sm-6"><p><span class="fa fa-euro-sign"></span>'+house.price+'</p></div><div class="col-sm-6"><p>'+house.size+'m<sup>2</sup></p></div></div><div class="row row-padding"><div class="col-sm-12"><button type="button" class="btn btn-primary btn-lg btn-block" id="primary-button">VIEW DETAILS</button></div></div></div>'	
+		'<div class="col col-sm-4"><img src="https://www.1limburg.nl/sites/default/files/public/styles/media-paragraph/public/macy-miller-tiny-house-2.jpg?itok=ySmiqYLu" class="img-fluid img-thumbnail"/><div class="row row-padding"><div class="col-sm-6"><p><span class="fa fa-map-pin"></span>'+house.city+'</p></div><div class="col-sm-6"><p>'+house.longitude + ', ' + house.latitude + '</p></div></div><div class="row row-padding"><div class="col-sm-6"><p><span class="fa fa-euro-sign"></span>'+house.price+'</p></div><div class="col-sm-6"><p>'+house.size+'m<sup>2</sup></p></div></div><div class="row row-padding"><div class="col-sm-12"><button type="button" class="btn btn-lg btn-block" id="primary-button">VIEW DETAILS</button></div></div></div>'	
 	);
 	if(i%3 == 0) {
 		$('.search-content-container').append('</div><div class="row row-padding>');
