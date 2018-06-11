d55d7f42ae6cb2105bb4cbcdf54115ad1a4d7234
Arctic Crow (Daniel) <arctic-crow@web.de>
2018-06-11 11:58:32 +0200

    Fixed content loading (see navigations.js) and duplicate variable declarations in search script; cleaned up code a little and converted to EMCA6 standard; (part 2)

--- js/global-variables.js
+++ js/global-variables.js
@@ -1,6 +1,20 @@
+// Search script variables
 let houseData = '';
 let priceSlider;// = document.getElementById("priceSlider");
 let priceOutput;// = document.getElementById("priceText");
 let sizeSlider;// = document.getElementById("distanceSlider");
 let sizeOutput;// = document.getElementById("distanceText");
 let cityOnly, innerCity, outerCity, showOptions = false;
+
+// Login script variables
+let isLoggedIn;
+let loggedInUser;
+
+let config = {
+    apiKey: "AIzaSyC3jpBVkEo3hupDE70BpIQC_efZ7hcyDIk",
+    authDomain: "login-70071.firebaseapp.com",
+    databaseURL: "https://login-70071.firebaseio.com",
+    projectId: "login-70071",
+    storageBucket: "login-70071.appspot.com",
+    messagingSenderId: "852685215526"
+};
